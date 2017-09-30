60e78d2604bb494dd0a6bbd291bd0a59e3111b64
cleverbeagle <business@cleverbeagle.com>
2017-09-30 09:55:11 -0500

    expand object structure to make tests a little easier to read

--- imports/modules/get-oauth-profile.test.js
+++ imports/modules/get-oauth-profile.test.js
@@ -2,21 +2,62 @@ import getOAuthProfile from './get-oauth-profile';
 
 test('Returns Google profile data properly formatted.', () => {
   const googleProfile = getOAuthProfile({}, {
-    services: { google: { email: 'example@test.com', given_name: 'Andy', family_name: 'Warhol' } },
+    services: {
+      google: {
+        email: 'admin@admin.com',
+        given_name: 'Andy',
+        family_name: 'Warhol',
+      },
+    },
   });
-  expect(googleProfile).toEqual({ email: 'example@test.com', name: { first: 'Andy', last: 'Warhol' } });
+
+  expect(googleProfile).toEqual({
+    email: 'admin@admin.com',
+    name: {
+      first: 'Andy',
+      last: 'Warhol',
+    },
+  });
 });
 
 test('Returns GitHub profile data properly formatted.', () => {
-  const githubProfile = getOAuthProfile({ profile: { name: 'Andy Warhol' } }, {
-    services: { github: { email: 'example@test.com' } },
+  const githubProfile = getOAuthProfile({
+    profile: {
+      name: 'Andy Warhol',
+    },
+  }, {
+    services: {
+      github: {
+        email: 'admin@admin.com',
+      },
+    },
   });
-  expect(githubProfile).toEqual({ email: 'example@test.com', name: { first: 'Andy', last: 'Warhol' } });
+
+  expect(githubProfile).toEqual({
+    email: 'admin@admin.com',
+    name: {
+      first: 'Andy',
+      last: 'Warhol',
+    },
+  });
 });
 
 test('Returns Facebook profile data properly formatted.', () => {
   const facebookProfile = getOAuthProfile({}, {
-    services: { facebook: { email: 'example@test.com', first_name: 'Andy', last_name: 'Warhol' } },
+    services: {
+      facebook: {
+        email: 'admin@admin.com',
+        first_name: 'Andy',
+        last_name: 'Warhol',
+      },
+    },
   });
-  expect(facebookProfile).toEqual({ email: 'example@test.com', name: { first: 'Andy', last: 'Warhol' } });
+
+  expect(facebookProfile).toEqual({
+    email: 'admin@admin.com',
+    name: {
+      first: 'Andy',
+      last: 'Warhol',
+    },
+  });
 });
